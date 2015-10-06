4e40a3ffe0ba39bfe07ab8df860d713a88fccada
HiznyakNikita <hiznyaknikita1996@gmail.com>
2015-10-06 09:41:37 +0300

    add new download view

---
+++ DownloadsManager/DownloadsManager/Views/NewDownloadView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DownloadsManager.Views
+{
+    /// <summary>
+    /// Interaction logic for NewDownloadView.xaml
+    /// </summary>
+    public partial class NewDownloadView : Window
+    {
+        private string urlToDownload;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public NewDownloadView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Uri for download
+        /// </summary>
+        public Uri UrlToDownload
+        {
+            get
+            {
+                return new Uri(urlToDownload);
+            }
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            urlToDownload = tbUrlToDownload.Text;
+            this.Close();
+        }
+    }
+}
