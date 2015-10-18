b92e12d7a2ce631b47d4c1b3ff7e04217257f999
HiznyakNikita <hiznyaknikita1996@gmail.com>
2015-10-18 13:25:56 +0300

    add segment calculator tests add skip resume segment tests

--- DownloadsManager/DownloadsManager.Tests/DownloaderTest.cs
+++ DownloadsManager/DownloadsManager.Tests/DownloaderTest.cs
@@ -65,8 +65,41 @@ namespace DownloadsManager.Tests
             download.Pause();
             Thread.Sleep(2000);
             download.Start();
-            Thread.Sleep(25000);
+            Thread.Sleep(30000);
             Assert.AreEqual(typeof(DownloadEndedState), download.State.GetType());
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.jpg");
+        }
+
+        //Test for one segment download, we must skip segment and start download from 0 if we paused
+        [TestMethod]
+        public void SkipOneSegmentIfPausedTest()
+        {
+            ResourceInfo resourceInfo = new ResourceInfo();
+            resourceInfo.Url = "https://www.dropbox.com/s/8lbatjtdues8mxh/ManagerVk_30_06_15.rar?dl=1";
+            Downloader download = new Downloader(
+                resourceInfo, null, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 1, "test.rar");
+            DownloaderManager.Instance.Add(download, true);
+            Thread.Sleep(20000);
+            download.Pause();
+            Thread.Sleep(2000);
+            Assert.AreEqual(0, download.FileSegments[0].Progress);
+            Assert.AreEqual(0, download.FileSegments[0].StartPosition);
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.rar");
+        }
+
+        //Test for one segment download, we must start from second(or more) segment if we paused
+        [TestMethod]
+        public void StartFromNextSegmentIfPausedTest()
+        {
+            ResourceInfo resourceInfo = new ResourceInfo();
+            resourceInfo.Url = "http://victoriafallsriverlodge.com/wp-content/uploads/2013/09/Lodge-from-walkway.jpg";
+            Downloader download = new Downloader(
+                resourceInfo, null, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 4, "test.jpg");
+            DownloaderManager.Instance.Add(download, true);
+            Thread.Sleep(10000);
+            download.Pause();
+            Thread.Sleep(2000);
+            Assert.AreEqual(100, download.FileSegments[0].Progress);
             File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.jpg");
         }
     }
---
+++ DownloadsManager/DownloadsManager.Tests/SegmentsCalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿using DownloadsManager.Core.Abstract;
+using DownloadsManager.Core.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadsManager.Tests
+{
+    [TestClass]
+    public class SegmentsCalculatorTests
+    {
+        private static List<CalculatedFileSegment> GetSegmentsFromCalculator()
+        {
+            IFileSegmentCalculator calculator = new FileSegmentSizeCalculatorHelper();
+            RemoteFileInfo remoteFileInfo = new RemoteFileInfo();
+            remoteFileInfo.AcceptRanges = true;
+            remoteFileInfo.FileSize = 4724748;
+            int segmentCount = 5;
+            List<CalculatedFileSegment> segments = calculator.GetSegments(segmentCount,remoteFileInfo);
+            return segments;
+        }
+
+        [TestMethod]
+        public void TestSegmentSizeCalculation()
+        {
+            List<CalculatedFileSegment> segments = GetSegmentsFromCalculator();
+            Assert.AreEqual(segments[0].SegmentEndPosition, 944949);
+        }
+
+        //Test for calculation size of last segment if we have some residue bytes after dividing
+        [TestMethod]
+        public void TestLastSegmentSizeCalculation()
+        {
+            List<CalculatedFileSegment> segments = GetSegmentsFromCalculator();
+            Assert.AreEqual(segments[4].SegmentEndPosition - segments[4].SegmentStartPosition, 944952);
+        }
+
+        //Test recalculation of segment size if segment size less than minimum in settings
+        [TestMethod]
+        public void TestMinimumSegmentSizeCalculation()
+        {
+            IFileSegmentCalculator calculator = new FileSegmentSizeCalculatorHelper();
+            RemoteFileInfo remoteFileInfo = new RemoteFileInfo();
+            remoteFileInfo.AcceptRanges = true;
+            remoteFileInfo.FileSize = 1773484;
+            int segmentCount = 6;
+            List<CalculatedFileSegment> segments = calculator.GetSegments(segmentCount, remoteFileInfo);
+            Assert.AreEqual(segments[0].SegmentEndPosition, 354696);
+        }
+    }
+}
