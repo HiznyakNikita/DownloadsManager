e4f94898b4486971ee832ed6d8ae443ade35d927
HiznyakNikita <hiznyaknikita1996@gmail.com>
2015-11-27 01:07:25 +0200

    add some comments

--- DownloadsManager/DownloadsManager.Tests/UiHelpersTest.cs
+++ DownloadsManager/DownloadsManager.Tests/UiHelpersTest.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace DownloadsManager.Tests
 {
+    /// <summary>
+    /// Tests for UI converters
+    /// </summary>
     [TestClass]
     public class UiHelpersTest
     {
